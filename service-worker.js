72a03caf30eab2ed1d454add6e2ed97097a4069d
Soroush Saki <63757011+soroush-saki@users.noreply.github.com>
2025-12-05 00:23:31 +0330

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'nevai-v1';
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap' 
+  // Add other local assets if any (like your Groq API client library if used)
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Try to return the cached response, otherwise fetch from network
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
